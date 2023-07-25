a32e1ff24801e39402d1a2c65cde4b747e80f15a
Slav Pisarov <115532039+slavpisarov@users.noreply.github.com>
2023-07-25 13:57:42 +0300

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule, createComponent } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CreateComponent } from './create/create.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+  {
+    path:'home',
+    component: HomeComponent,
+  },
+  {
+    path:'catalog',
+    component: CatalogComponent,
+  },  
+  {
+    path:'create',
+    component: CreateComponent,
+  },
+  {
+    path:'user',
+    loadChildren: ()=> import('./user/user.module').then((m) => m.UserModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CreateComponent } from './create/create.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CatalogComponent,
+    CreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
